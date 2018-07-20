a309fed5a8df20cb1c1902451985a7d8f772037c
michaelcraig <m_craig@baylor.edu>
2018-07-20 12:11:07 -0400

    waves move, very basic stages

---
+++ app.js
@@ -0,0 +1,33 @@
+var infiniteLoopAlternate = anime({
+  targets: '#infiniteLoopAlternate .firstWave',
+  translateY: -50,
+  direction: 'reverse',
+  duration: 4000,
+});
+
+var infiniteLoopAlternate2 = anime({
+  targets: '#infiniteLoopAlternate .secondWave',
+  translateY: -55,
+  direction: 'alternate',
+  loop: true,
+  duration: 4000,
+  delay: 1500
+});
+
+var infiniteLoopAlternate3 = anime({
+  targets: '#infiniteLoopAlternate .thirdWave',
+  translateY: -75,
+  direction: 'alternate',
+  loop: true,
+  duration: 4000,
+  delay: 2500
+});
+
+var infiniteLoopAlternate4 = anime({
+  targets: '#infiniteLoopAlternate .fourthWave',
+  translateY: -65,
+  direction: 'alternate',
+  loop: true,
+  duration: 4000,
+  delay: 3500
+});
